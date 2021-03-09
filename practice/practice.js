7ac8847f53b5219bc8faf21c2e0e16d80a40e256
Unknown <ljw041101@gmail.com>
2021-03-09 23:30:59 +0900

    typescript 타입 추론

--- practice/practice.js
+++ practice/practice.js
@@ -1,4 +1,10 @@
-function logName(name) {
-    console.log(name);
-}
-logName('Lee');
+var student = {
+    name: 'Lee',
+    age: 18,
+    course: 'Fighting Typescript',
+    codingIQ: 0,
+    code: function () {
+        console.log('brain is not worknig');
+    }
+};
+student.age = 18.5;
--- practice/practice.ts
+++ practice/practice.ts
@@ -1,5 +1,12 @@
-function logName (name: string){
-    console.log(name);
+let student = {
+    name: 'Lee',
+    age: 18,
+    course: 'Fighting Typescript',
+    codingIQ: 0,
+    code: function(){
+        console.log('brain is not worknig');
+    }
 }
-
-logName('Lee');
+//typescript에서 이미 프로퍼티 값의 타입을 파악 ex) name = string
+//name은 string이기 때문에 string 이외의 값은 들어갈 수 없음
+student.age = 18.5;
