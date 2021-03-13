0df274e14fc9dab6ed373c447f85bbab96f5fba2
Unknown <ljw041101@gmail.com>
2021-03-13 23:37:10 +0900

    유니언 타입, 타입가드, 타입 별칭

--- practice/practice.js
+++ practice/practice.js
@@ -1,42 +1,21 @@
-var GenderType;
-(function (GenderType) {
-    GenderType["male"] = "male";
-    GenderType["female"] = "female";
-})(GenderType || (GenderType = {}));
-//숫자 열거형에 반대된 문자형 열거형이 값을 알아보기 편하다는 장점이 있음
-var student = {
-    name: 'Lee',
-    age: 18,
-    course: 'Fighting Typescript',
-    codingIQ: 0,
-    code: function () {
-        console.log('brain is not worknig');
+var anyType;
+//타입스크립트는 타입에 관한 더 많은 정보를 제시할수록 좋다.
+var price;
+price: 3;
+price: 'goldmedal';
+var totalCost;
+var orderID;
+var calculateTotalCost = function (price, qty) {
+};
+var findOrderID = function (customer, productId) {
+    return orderID;
+};
+var setItemPrice = function (variable) {
+    if (typeof variable === 'string') {
+        totalCost = 0;
+    }
+    else {
+        totalCost = variable;
     }
 };
-//typescript에서 이미 프로퍼티 값의 타입을 파악 ex) name = string
-//name은 string이기 때문에 string 이외의 값은 들어갈 수 없음
-student.age = 18.5;
-var age = 18;
-//객체를 인터페이스로 지정
-//프로퍼티명 뒤에 ?를 사용하여 선택적 프로퍼티로 만들어준다.
-function getStudent(studentID) {
-    return {
-        studentID: 2216,
-        studentName: 'Lee',
-        gender: 'male',
-        subject: 'Web',
-        courseCompleted: true
-    };
-}
-//인터페이스가 반환값이기 때문에 리턴에서 인터페이스의 모든 요소를 반환하여야 한다.
-var student1 = {
-    studentID: 2215,
-    studentName: 'Lee',
-    age: 18,
-    gender: GenderType.male,
-    subject: 'Server',
-    courseCompleted: false
-};
-function saveStudent(student) {
-}
-saveStudent(student1);
+setItemPrice(50);
--- practice/practice.ts
+++ practice/practice.ts
@@ -1,56 +1,23 @@
-enum GenderType{
-    male="male",
-    female="female"
-}
-//숫자 열거형에 반대된 문자형 열거형이 값을 알아보기 편하다는 장점이 있음
+let anyType: any;
+//타입스크립트는 타입에 관한 더 많은 정보를 제시할수록 좋다.
+let price: number | string;
+price: 3;
+price: 'goldmedal';
 
-let student = {
-    name: 'Lee',
-    age: 18,
-    course: 'Fighting Typescript',
-    codingIQ: 0,
-    code: function(){
-        console.log('brain is not worknig');
+type StrOrNum = number | string;
+let totalCost: number;
+let orderID: StrOrNum;
+const calculateTotalCost = (price: StrOrNum, qty: number):void=>{
+};
+
+const findOrderID = (customer:{costomerID: StrOrNum, name:string}, productId: StrOrNum): StrOrNum =>{
+    return orderID;
+};
+const setItemPrice = (variable: StrOrNum): void =>{
+    if(typeof variable === 'string'){
+        totalCost = 0;
+    } else{
+        totalCost = variable;
     }
-}
-//typescript에서 이미 프로퍼티 값의 타입을 파악 ex) name = string
-//name은 string이기 때문에 string 이외의 값은 들어갈 수 없음
-student.age = 18.5;
-
-let age:number = 18;
-//타입 명시로 age가 number임을 명시함
-interface Student{
-    studentID: number;
-    studentName: string;
-    age?: number;
-    gender: 'male' | 'female';
-    subject: string;
-    courseCompleted: boolean;
-    addComment?:(comment:string) => string;
-    readonly phoneNumber?: number;
-}
-//객체를 인터페이스로 지정
-//프로퍼티명 뒤에 ?를 사용하여 선택적 프로퍼티로 만들어준다.
-function getStudent(studentID: number): Student{
-    return{
-        studentID: 2216,
-        studentName: 'Lee',
-        gender: 'male',
-        subject: 'Web',
-        courseCompleted: true,
-    }
-}
-//인터페이스가 반환값이기 때문에 리턴에서 인터페이스의 모든 요소를 반환하여야 한다.
-
-let student1= {
-    studentID: 2215,
-    studentName: 'Lee',
-    age: 18,
-    gender: GenderType.male,
-    subject: 'Server',
-    courseCompleted: false,
-}
-function saveStudent (student: Student): void{
-}
-
-saveStudent(student1);
+};
+setItemPrice(50);
