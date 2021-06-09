e2645596908e148db411c3cd531ddc075c3950b3
Unknown <ljw041101@gmail.com>
2021-06-09 23:06:01 +0900

    enum

--- practice/practice.ts
+++ practice/practice.ts
@@ -7,7 +7,7 @@ enum num {
 enum에 값을 할당하지 않으면 0이 된다. -> zero = 0
 값이 할당되어 있고 그 다음값이 할당되지 않으면 그 전 값에 +1을 더한다. -> two = 2
 */
-console.log(num.zero, num.one, num.two); // 0, 1, 2
+console.log(num.zero, num.one, num.two); // 0(값을 할당하지 않았기 때문), 1, 2
 
 /* 클래스의 접근 범위
 public: 외부, 상속 둘 다 접근 가능합니다.
